d26b957a12198d26eede19d81405c90690cb40fc
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-01-13 02:13:55 +0530

    fix: correct async file deletion logic and HTML/CSS errors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs/promises');
 
 const app = express();
 const port = 3009;
@@ -12,25 +12,28 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-app.post('/delete-file', (req, res) => {
+app.post('/delete-file', async (req, res) => {
     const fileName = req.body.filename;
     const filePath = path.join(__dirname, fileName);
 
     let message = '';
 
     try {
-        fs.unlink(filePath, (err)=>{});
-        message = `<p style="green">File ${fileName} deleted successfully.`;
+        await fs.unlink(filePath);
+        message = `<p style="color: green">File ${fileName} deleted successfully.`;
     }
     catch (err) {
-        if (err.code === 'ENOENT') message = `<p style="orange">File ${fileName} NOT FOUND!`;
-        message = `<p style="red">Error deleting file ${fileName}: ${err.message}`
+        if (err.code === 'ENOENT') {
+            message = `<p style="color: orange">File ${fileName} NOT FOUND!</p>`;
+        }
+        else {
+            message = `<p style="color: red">Error deleting file ${fileName}: ${err.message}</p>`;
+        }
     }
 
-    fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
-        const finalHTML = data.replace('<p id="confirmation-message"></p>', `<p id="confirmation-message">${message}</p>`);
-        res.send(finalHTML);
-    })
+    const data = await fs.readFile(path.join(__dirname, 'index.html'), 'utf8');
+    const finalHTML = data.replace('<p id="confirmation-message"></p>', `<p id="confirmation-message">${message}</p>`);
+    res.send(finalHTML);
 
 })
 
